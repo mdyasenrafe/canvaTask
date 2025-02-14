3413ca4eaa0e00d9a479dbb7d0663c54b08effd4
Md yasen rafe <mdyasenrafe@gmail.com>
2025-02-15 01:21:21 +0600

    navigation working

--- src/theme/elements/textVariants.ts
+++ src/theme/elements/textVariants.ts
@@ -5,39 +5,32 @@ export const BASE_TEXT_VARIANTS = {
   defaults: {
     fontSize: 14,
     fontFamily: FONTS.regular,
-    color: 'text',
   },
   h3: {
     fontSize: 30,
     fontWeight: 700,
     fontFamily: FONTS.bold,
-    color: 'text',
   },
   h4: {
     fontSize: 24,
     fontWeight: 700,
     fontFamily: FONTS.bold,
-    color: 'text',
   },
   buttonLabel: {
     fontSize: 20,
     fontFamily: FONTS.semiBold,
-    color: 'text',
   },
   buttonWhiteLabel: {
     fontSize: 20,
     fontFamily: FONTS.semiBold,
-    color: 'white',
   },
   buttonPrimaryLabel: {
     fontSize: 20,
     fontFamily: FONTS.semiBold,
-    color: 'primary',
   },
   p1: {
     fontSize: 20,
     fontFamily: FONTS.regular,
-    color: 'text',
   },
   p1_white: {
     fontSize: 20,
@@ -47,17 +40,14 @@ export const BASE_TEXT_VARIANTS = {
   p2: {
     fontSize: 18,
     fontFamily: FONTS.regular,
-    color: 'text',
   },
   p3: {
     fontSize: 16,
     fontFamily: FONTS.regular,
-    color: 'text',
   },
   p4: {
     fontSize: 14,
     fontFamily: FONTS.regular,
-    color: 'text',
   },
   p4_white: {
     fontSize: 14,
@@ -67,37 +57,30 @@ export const BASE_TEXT_VARIANTS = {
   p5: {
     fontSize: 12,
     fontFamily: FONTS.regular,
-    color: 'text',
   },
   p6: {
     fontSize: 10,
     fontFamily: FONTS.regular,
-    color: 'text',
   },
   p1_bold: {
     fontSize: 20,
     fontFamily: FONTS.bold,
-    color: 'text',
   },
   p1_bold_white: {
     fontSize: 20,
     fontFamily: FONTS.bold,
-    color: 'white',
   },
   p1_medium: {
     fontSize: 20,
     fontFamily: FONTS.medium,
-    color: 'text',
   },
   p3_medium: {
     fontSize: 16,
     fontFamily: FONTS.medium,
-    color: 'text',
   },
   p3_bold: {
     fontSize: 16,
     fontFamily: FONTS.bold,
-    color: 'text',
   },
   p3_medium_white: {
     fontSize: 16,
@@ -112,17 +95,10 @@ export const BASE_TEXT_VARIANTS = {
   p3_medium_primary: {
     fontSize: 16,
     fontFamily: FONTS.medium,
-    color: 'red',
   },
   p4_bold: {
     fontSize: 16,
     fontFamily: FONTS.bold,
-    color: 'text',
-  },
-  p4_primary: {
-    fontSize: 14,
-    fontFamily: FONTS.regular,
-    color: 'red',
   },
   p4_black: {
     fontSize: 14,
--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -6,80 +6,11 @@ import {
   BASE_TEXT_VARIANTS,
 } from './elements';
 
-export const lightTheme = createTheme({
-  colors: {
-    ...BASE_COLORS,
-    background: BASE_COLORS.white,
-    text: BASE_COLORS.secondary,
-    placeholder: 'grey',
-  },
+export const theme = createTheme({
+  colors: BASE_COLORS,
   spacing: BASE_SPACING,
   textVariants: BASE_TEXT_VARIANTS,
   borderRadii: BASE_BORDER_RADII,
-  textInputVariants: {
-    defaults: {
-      color: 'black',
-    },
-    rounded: {
-      borderRadius: 'roundedInput',
-    },
-  },
-  snackbarVariants: {
-    defaults: {},
-    error: {
-      backgroundColor: 'red',
-    },
-    success: {
-      backgroundColor: 'green',
-    },
-  },
-  mentionInputVariants: {
-    defaults: {
-      borderRadius: 'smallRoundedInput',
-      borderColor: 'red',
-      borderWidth: 1,
-      px: 'md',
-    },
-  },
 });
 
-export const darkTheme = createTheme({
-  colors: {
-    ...BASE_COLORS,
-    primary: BASE_COLORS.secondary,
-    background: BASE_COLORS.black,
-    text: BASE_COLORS.white,
-    placeholder: 'white',
-  },
-  spacing: BASE_SPACING,
-  textVariants: BASE_TEXT_VARIANTS,
-  borderRadii: BASE_BORDER_RADII,
-  textInputVariants: {
-    defaults: {
-      color: 'white',
-    },
-    rounded: {
-      borderRadius: 'roundedInput',
-      borderColor: '',
-    },
-  },
-  snackbarVariants: {
-    defaults: {},
-    error: {
-      backgroundColor: 'red',
-    },
-    success: {
-      backgroundColor: 'green',
-    },
-  },
-  mentionInputVariants: {
-    defaults: {
-      borderRadius: 'smallRoundedInput',
-      borderColor: 'red',
-      borderWidth: 1,
-      px: 'md',
-    },
-  },
-});
-
-export type Theme = typeof lightTheme;
+export type Theme = typeof theme;
